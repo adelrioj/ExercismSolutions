4646261f4641a8e8e928c543535bb641e08708de
adelrioj <adelrioj@gmail.com>
2016-07-15 10:55:37 +0200

    Added anagram exercise, unsolved

---
+++ java/anagram/src/main/java/Anagram.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Anagram {
+
+    private String anagram;
+
+    public Anagram(String anagram) {
+        this.anagram = anagram;
+    }
+
+    public List<String> match(List<String> anagrams) {
+
+        return anagrams;
+    }
+}
