0564f01436f3b8cd8784af61cb554d51e94310f7
adelrioj <adelrioj@gmail.com>
2016-07-15 10:55:11 +0200

    changed pangram to start from alphabet stream instead of pangram char stream.

--- java/pangram/src/main/java/Pangrams.java
+++ java/pangram/src/main/java/Pangrams.java
@@ -1,15 +1,14 @@
-import java.util.function.Predicate;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Pangrams {
 
-    public static final int ALPHABET_CHAR_COUNT = 26;
-
     public static boolean isPangram(String pangram) {
-        Predicate<Character> matchAlphabetChars = c -> c.toString().matches("[a-zA-Z]");
-
         String lowerCasePangram = pangram.toLowerCase();
-        Stream<Character> pangramAsCharStream = lowerCasePangram.chars().mapToObj(i -> (char) i);
-        return pangramAsCharStream.filter(matchAlphabetChars).distinct().count() == ALPHABET_CHAR_COUNT;
+        return getLowerCaseAlphabetAsCharStream().allMatch(c -> lowerCasePangram.contains(Character.toString(c)));
+    }
+
+    private static Stream<Character> getLowerCaseAlphabetAsCharStream(){
+        return IntStream.rangeClosed('a','z').mapToObj(i -> (char)i);
     }
 }
