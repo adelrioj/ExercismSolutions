b31b5d8c5b1476afb91b7c38b6126094cad41b0a
adelrioj <adelrioj@gmail.com>
2016-07-18 12:25:03 +0200

    Added solution to anagram exercise

--- java/anagram/src/main/java/Anagram.java
+++ java/anagram/src/main/java/Anagram.java
@@ -1,15 +1,45 @@
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Anagram {
 
-    private String anagram;
+    public static final int CHARS_IN_ALPHABET = 26; //26 == chars in english alphabet
+    private String word;
 
-    public Anagram(String anagram) {
-        this.anagram = anagram;
+    public Anagram(String word) {
+        this.word = word;
     }
 
     public List<String> match(List<String> anagrams) {
+        return anagrams.stream().filter(this::isAnagram).collect(Collectors.toList());
+    }
 
-        return anagrams;
+    private boolean isAnagram(String possibleAnagram){
+        if (hasDifferentLenght(possibleAnagram))
+            return false;
+        if (isSameWord(possibleAnagram))
+            return false;
+
+        char[] wordCharArray = word.toLowerCase().toCharArray();
+        char[] possibleAnagramCharArray = possibleAnagram.toLowerCase().toCharArray();
+        int[] counts = new int[CHARS_IN_ALPHABET];
+        for (int i = 0; i < wordCharArray.length; i++){
+            counts[wordCharArray[i]-97]++;  // Increment the count of the character at i
+            counts[possibleAnagramCharArray[i]-97]--;  // Decrement the count of the character at i
+        }
+
+        for (int i = 0; i<26; i++)
+            if (counts[i] != 0)
+                return false;
+
+        return true;
+    }
+
+    private boolean isSameWord(String possibleAnagram) {
+        return word.equalsIgnoreCase(possibleAnagram);
+    }
+
+    private boolean hasDifferentLenght(String possibleAnagram) {
+        return word.length() != possibleAnagram.length();
     }
 }
