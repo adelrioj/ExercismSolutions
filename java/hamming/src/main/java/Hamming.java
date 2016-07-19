4fe37006ca5c0dcd863f035627e52310152aa004
adelrioj <adelrioj@gmail.com>
2016-07-19 10:09:25 +0200

    Solved Hamming exercise w/ java 8 streams

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,2 +1,20 @@
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
 public class Hamming {
+
+    public static int compute(String strand1, String strand2) {
+        if (strand1.length() != strand2.length())
+            throw new IllegalArgumentException();
+
+        long differences = IntStream.range(0, strand1.length())
+                .filter(compareNucleotidesByPosition(strand1, strand2))
+                .count();
+
+        return (int) differences;
+    }
+
+    private static IntPredicate compareNucleotidesByPosition(String strand1, String strand2) {
+        return i -> strand1.charAt(i) != strand2.charAt(i);
+    }
 }
--- java/hamming/src/test/java/HammingTest.java
+++ java/hamming/src/test/java/HammingTest.java
@@ -1,7 +1,7 @@
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class HammingTest {
     
