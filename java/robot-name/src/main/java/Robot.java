3e6d2218fa935dee7f19474fd3790f3d02e2e897
adelrioj <adelrioj@gmail.com>
2016-08-06 11:30:57 +0200

    added first, ugly solution to robot exercise

--- java/robot-name/src/main/java/Robot.java
+++ java/robot-name/src/main/java/Robot.java
@@ -1,16 +1,29 @@
-/**
- *
- * Created by adelrioj.
- */
+import java.util.Random;
+
 public class Robot {
 
     private String name;
+
+    public Robot() {
+        reset();
+    }
 
     public String getName() {
         return name;
     }
 
     public void reset() {
+        this.name  = generateName();
+    }
 
+    private String generateName(){
+        java.util.Random random = new Random();
+        char c1 = (char)(random.nextInt(26) + 'A');
+        char c2 = (char)(random.nextInt(26) + 'A');
+        char n1 = (char)(random.nextInt(9) + '0');
+        char n2 = (char)(random.nextInt(9) + '0');
+        char n3 = (char)(random.nextInt(9) + '0');
+
+        return String.valueOf(c1) + c2 + n1 + n2 + n3;
     }
 }
