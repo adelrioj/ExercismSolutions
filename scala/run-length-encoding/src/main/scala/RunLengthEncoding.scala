7a899fdb0c24184f8b4e4fbc7b35f73dfe892280
adelrioj <adelrioj@gmail.com>
2017-02-23 10:28:17 +0100

    first iteration on run-lenght-encoding exercise added scala config to gitignore

---
+++ scala/run-length-encoding/src/main/scala/RunLengthEncoding.scala
@@ -0,0 +1,32 @@
+object RunLengthEncoding {
+
+    def encode(str: String): String = {
+        def encodeList(strList: List[Char]): List[(Int, Char)] = strList match {
+            case Nil => Nil
+            case y :: ys => encodeList(ys) match {
+                case (c, `y`) :: rest => (c + 1, y) :: rest
+                case             rest => (    1, y) :: rest
+            }
+        }
+
+        encodeList(str.toList) map {
+            case (1, char) => s"$char"
+            case (int, char) => s"$int$char"
+        } mkString
+    }
+
+    def decode(str: String): String = {
+        val sb = new StringBuilder
+        val pattern =  """(\d*)([a-zA-Z\s])""".r
+
+        def checkLenght(l: String) = l match {
+            case "" => 1
+            case s => s.toInt
+        }
+
+        for (pattern(l, c) <- pattern findAllIn str){
+            sb.append(c * checkLenght(l))
+        }
+        sb.toString
+    }
+}
