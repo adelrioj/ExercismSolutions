68015ebca97e6d34c85c9f38dff271fa9aaa1ce9
adelrioj <adelrioj@gmail.com>
2017-02-28 17:13:20 +0100

    Another iteration on hamming execise

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -1,14 +1,26 @@
+
+object s_+: {
+    def unapply(s: String): Option[(Char, String)] = s.headOption map { (_, s.tail) }
+}
+
 object Hamming {
 
     def compute(dna1: String, dna2: String):Option[Int] = {
-        if (dna1.length != dna2.length)
-            None
-        else
-            Some(
-                dna1 zip dna2 count {
-                    case (a, b) => a != b
-                }
-            )
+
+        def checkValue[A](c1:A, c2:A) = if (c1 == c2) 0 else 1
+
+        def notConsistent(xs: String, ys: String) = {
+            (xs.isEmpty && ys.nonEmpty) || (xs.nonEmpty && ys.isEmpty)
+        }
+
+        def computeInRecLoop(xs: String, ys: String, acc: Int): Option[Int] =
+            (xs, ys) match {
+                case (`xs`, `ys`) if notConsistent(xs, ys) => None
+                case ("", "") => Some(acc)
+                case (x s_+: xTail, y s_+: yTail) => computeInRecLoop(xTail, yTail, acc + checkValue(x, y))
+            }
+
+        computeInRecLoop(dna1, dna2, 0)
     }
 
 }
