142cbb85f2e520c5fdcf60b406d4870bcbf719e7
adelrioj <adelrioj@gmail.com>
2017-02-28 18:51:03 +0100

    Added Java Meetup exercise, partially solved

--- java/meetup/src/main/java/Meetup.java
+++ java/meetup/src/main/java/Meetup.java
@@ -1,11 +1,41 @@
 
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+
 public class Meetup {
 
-    public Meetup(int a, int b){
+    private Integer year;
+    private Integer monthOfYear;
 
+    public Meetup(int monthOfYear, int year){
+        this.monthOfYear = monthOfYear;
+        this.year = year;
     }
 
-    public boolean day(int monday, MeetupSchedule teenth) {
-        return false;
+    public DateTime day(int dayOfWeek, MeetupSchedule schedule) {
+
+        if (MeetupSchedule.FIRST.equals(schedule)){
+            return getDateByWeek(dayOfWeek, 0);
+        } else if (MeetupSchedule.SECOND.equals(schedule)){
+            return getDateByWeek(dayOfWeek, 1);
+        } else if (MeetupSchedule.THIRD.equals(schedule)){
+            return getDateByWeek(dayOfWeek, 2);
+        } else if (MeetupSchedule.FOURTH.equals(schedule)){
+            return getDateByWeek(dayOfWeek, 3);
+        } else if (MeetupSchedule.LAST.equals(schedule)){
+            LocalDate localDate = new LocalDate(this.year, this.monthOfYear, 1)
+                    .withDayOfWeek(dayOfWeek);
+            return localDate.dayOfWeek().withMaximumValue().toDateTimeAtStartOfDay();
+        } else if (MeetupSchedule.TEENTH.equals(schedule)){
+
+        }
     }
+
+    private DateTime getDateByWeek(int dayOfWeek, int weekNumber) {
+        return new LocalDate(this.year, this.monthOfYear, 1)
+                .withDayOfWeek(dayOfWeek)
+                .plusWeeks(weekNumber)
+                .toDateTimeAtStartOfDay();
+    }
+
 }
