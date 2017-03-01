a24513574313b6769ade1e34b8f11a709e64e222
adelrioj <adelrioj@gmail.com>
2017-03-01 22:11:53 +0100

    Added solution for Scala word-count

--- scala/word-count/src/main/scala/Phrase.scala
+++ scala/word-count/src/main/scala/Phrase.scala
@@ -3,13 +3,14 @@ class Phrase(s: String) {
 
   def wordCount: Map[String, Int] = {
 
-    val pattern = "\\W+('\\W+)?".r
+    val pattern = """[^\w']+"""
 
     def checkForWord(map: Map[String, Int], word: String) = {
       val lcWord = word.toLowerCase
       map + (lcWord -> (map.getOrElse(lcWord, 0) + 1))
     }
 
-    pattern.findAllMatchIn(s).foldLeft(Map.empty[String, Int])(checkForWord)
+    s.split(pattern).foldLeft(Map.empty[String, Int])(checkForWord)
+
   }
 }
