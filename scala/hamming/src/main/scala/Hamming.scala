93538c919fbb94867a733b5406a82ea762cd9555
adelrioj <adelrioj@gmail.com>
2017-03-01 22:12:05 +0100

    Another iteration on Scala Hamming

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -1,23 +1,17 @@
-
-object s_+: {
-    def unapply(s: String): Option[(Char, String)] = s.headOption map { (_, s.tail) }
-}
+import scala.annotation.tailrec
 
 object Hamming {
 
     def compute(dna1: String, dna2: String):Option[Int] = {
 
-        def checkValue[A](c1:A, c2:A) = if (c1 == c2) 0 else 1
+        def distance[A](c1:A, c2:A) = if (c1 == c2) 0 else 1
 
-        def notConsistent(xs: String, ys: String) = {
-            (xs.isEmpty && ys.nonEmpty) || (xs.nonEmpty && ys.isEmpty)
-        }
-
-        def computeInRecLoop(xs: String, ys: String, acc: Int): Option[Int] =
+        @tailrec
+        def computeInRecLoop(xs: Seq[Char], ys: Seq[Char], acc: Int): Option[Int] =
             (xs, ys) match {
-                case (`xs`, `ys`) if notConsistent(xs, ys) => None
-                case ("", "") => Some(acc)
-                case (x s_+: xTail, y s_+: yTail) => computeInRecLoop(xTail, yTail, acc + checkValue(x, y))
+                case (Nil, Nil) => Some(acc)
+                case (x+:xTail, y+:yTail) => computeInRecLoop(xTail, yTail, acc + distance(x, y))
+                case _ => None
             }
 
         computeInRecLoop(dna1, dna2, 0)
